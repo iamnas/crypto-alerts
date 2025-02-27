4735cf624e87cab16f816e9dad9b9ffa6e73ba4c
iamnas <iamnas.eth@gmail.com>
2025-02-28 01:58:30 +0530

    update price fetching interval to every 5 minutes and implement price storage in database

--- src/price/price.controller.ts
+++ src/price/price.controller.ts
@@ -5,11 +5,10 @@ import { PriceService } from './price.service';
 @Controller('price')
 export class PriceController {
 
-    constructor(private priceService: PriceService) {}
-    @Cron(CronExpression.EVERY_30_SECONDS)
-    handleCron() {
-        const data = this.priceService.getPrice();
-        console.log(data);
-        
+    constructor(private priceService: PriceService ) {}
+    @Cron(CronExpression.EVERY_5_MINUTES)
+    async handleCron() {
+        await this.priceService.fetchAndStorePrices();   
     }
+
 }
--- src/price/price.service.ts
+++ src/price/price.service.ts
@@ -1,13 +1,31 @@
-import { Injectable } from '@nestjs/common';
-
-
+import { Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { MoralisService } from 'src/moralis/moralis.service';
 
 @Injectable()
 export class PriceService {
+    private readonly logger = new Logger(PriceService.name);
+
+    constructor(private readonly prisma: PrismaService, private readonly moralisService: MoralisService) { }
 
 
-    getPrice(): string {
-        return 'Hello World!';
+
+    async fetchAndStorePrices() {
+        try {
+            const ethPrice = await this.moralisService.getPrice('ethereum');
+            const maticPrice = await this.moralisService.getPrice('polygon');
+
+            await this.prisma.price.create({
+                data: { chain: 'ethereum', price: ethPrice },
+            });
+
+            await this.prisma.price.create({
+                data: { chain: 'polygon', price: maticPrice },
+            });
+
+            this.logger.log(`Saved Prices - ETH: $${ethPrice}, MATIC: $${maticPrice}`);
+        } catch (error) {
+            this.logger.error('Error fetching prices', error);
+        }
     }
- 
 }
